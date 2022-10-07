f68cbc927fdd5485acaba604654ddba792ed61d8
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-10-06 20:20:01 -0400

    reorganize the files and directory

---
+++ assets/js/choices.js
@@ -0,0 +1,49 @@
+let questions = [
+    {
+        question: "Arrays in JavaScript can be used to store _____ .",
+        answers: [
+            { choice: "booleans", correct: false},
+            { choice: "strings", correct: false},
+            { choice: "arrays", correct: false},
+            { choice: "all of the above", correct:true}
+        ]
+    }, 
+    {
+        question: "The condition in an if/ else statement is enclosed within _____ .",
+        answers: [
+            { choice: "quotes", correct: false},
+            { choice: "curly brackets", correct: false},
+            { choice: "parentheses", correct: true},
+            { choice: "square brackets", correct: false}
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is _____ . ",
+        answers: [
+            { choice: "JavaScript", correct: false},
+            { choice: "console.log", correct: true},
+            { choice: "terminal", correct: false},
+            { choice: "GitHub Pages", correct: true}
+        ]
+    },
+    {
+        question: "How often should you commit changes to your code?",
+        answers: [
+            { choice: "early and often", correct: true},
+            { choice: "never", correct: false},
+            { choice: "once a month", correct: false},
+            { choice: "twice a week", correct: false}
+        ]
+    },
+    {
+        question: "Which of the following is an example of a closing tag?",
+        answers: [
+            {choice: "<html>", correct: false},
+            {choice: "</div>", correct: true},
+            {choice: "span", correct: false},
+            {choice: "<br>", correct: false}
+        ]
+
+    }
+];
+
